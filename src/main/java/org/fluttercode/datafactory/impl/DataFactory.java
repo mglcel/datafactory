700f3eeef6bd0435871661fa6981fad2fdc671bb
Loïc Dias Da Silva <mglcel@mglcel.fr>
2015-09-10 12:25:00 +0200

    better random

--- src/main/java/org/fluttercode/datafactory/impl/DataFactory.java
+++ src/main/java/org/fluttercode/datafactory/impl/DataFactory.java
@@ -45,7 +45,7 @@ import org.fluttercode.datafactory.NameDataValues;
 public final class DataFactory {
 
   // used for backwards compatibility
-  private static Random original_random = new Random(93285);
+  private static Random original_random = new Random((int)(Math.random() * 100000));
 
   private NameDataValues nameDataValues = new DefaultNameDataValues();
   private AddressDataValues addressDataValues = new DefaultAddressDataValues();
